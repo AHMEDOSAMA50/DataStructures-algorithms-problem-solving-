079ebb8d09d6a30abfa1f3b5759e79e2243e5521
Ahmed Osama <94145850+a-Klevy@users.noreply.github.com>
2022-05-22 20:41:49 +0200

    Time: 178 ms (12.43%), Space: 51.3 MB (68.70%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.cpp
+++ 217-contains-duplicate/217-contains-duplicate.cpp
@@ -1,13 +1,14 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-    sort(nums.begin(), nums.end());
-        //we started from 1 cause we want to compare each element                                                  with the previous one as the vector is sorted
-        for(int i = 1;i<nums.size();i++){   
-            if(nums[i-1]==nums[i]){
+        
+        
+    unordered_set<int> set;
+        for(int num : nums){
+            if(set.find(num)!= set.end()){
                 return true;
             }
-            
+            set.insert(num);
         }
         return false;
     }
